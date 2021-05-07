8aab343cd20ab1b9639b15fb72c4faabaadc70f5
viniVendrame <52543661+viniVendrame@users.noreply.github.com>
2021-05-07 17:37:40 -0300

    Correções

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const tabMenu = () =>{
+const tabMenu = () => {
     const equipImg = document.querySelectorAll('.equip-list li')
     const equipDescription = document.querySelectorAll('.equip-description section')
     equipDescription[0].classList.add('ativo')
 
-    function showSection(index){
-        equipDescription.forEach((item) =>{
+    function showSection(index) {
+        equipDescription.forEach((item) => {
             item.classList.remove('ativo')
         })
         equipDescription[index].classList.add('ativo')
     }
 
-    equipImg.forEach((item, index) =>{
-        item.addEventListener('click', () =>{
+    equipImg.forEach((item, index) => {
+        item.addEventListener('click', () => {
             showSection(index)
         })
     })
 }
 
 
-const navBar = () =>{
+const navBar = () => {
     const nav = document.querySelector('.menu')
 
-    window.addEventListener('scroll', () =>{
-        if(window.scrollY > 0){
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
             nav.classList.add('navFixed')
-        } else{
+        } else {
             nav.classList.remove('navFixed')
         }
     })
 }
 
-const burgerMenu = () =>{
+const burgerMenu = () => {
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu-list')
     const menuLinks = document.querySelectorAll('.menu-list li')
 
-    burger.addEventListener('click', () =>{
+    burger.addEventListener('click', () => {
         menu.classList.toggle('ativo')
 
-        menuLinks.forEach((item, index) =>{
-            if(item.style.animation){
+        menuLinks.forEach((item, index) => {
+            if (item.style.animation) {
                 item.style.animation = ''
             } else {
-                item.style.animation = `navLinkFade .5s ease forwards ${index /7 + .3}s`
+                item.style.animation = `navLinkFade .5s ease forwards ${index / 7 + .3}s`
             }
         })
 
@@ -50,7 +50,7 @@ const burgerMenu = () =>{
     })
 }
 
-const allFunction = () =>{
+const allFunction = () => {
     tabMenu()
     navBar()
     burgerMenu()
